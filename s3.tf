088043150d15cb48e8a2f2ce3d5483bcb4fc9ef9
Hardik Sharma <124352235+hsharma81@users.noreply.github.com>
2023-12-11 14:36:31 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_version = "~> 1.5"
+
+  backend "s3" {
+    region = "eu-west-1"
+    bucket = "terraform-examples"
+    key    = "terraform/atlantis/poc.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.30"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
 
