a417dabc110b4b0258b24850a107432c284d2710
hsharma81 <hardik.sharma@gojokotech.com>
2023-12-11 13:31:32 +0530

    -

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_version = "~> 1.5"
+
+  backend "s3" {
+    region = "eu-west-1"
+    bucket = "terraform-examples"
+    key    = "terraform/atlantis/poc.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.3.0"
+    }
+  }
 }
 
+
+resource "aws_s3_bucket" "bucket" {
+	bucket = "atlantis-resource-bucket"
+	}
+resource "null_resource" "cluster" {
+}
